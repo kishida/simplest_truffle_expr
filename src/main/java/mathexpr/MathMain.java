052f1bffd8f8e9686411f205940e74eb3cecc110
kishida <naokikishida@gmail.com>
2019-01-04 17:47:25 +0900

    Run as command

--- src/main/java/mathexpr/MathMain.java
+++ src/main/java/mathexpr/MathMain.java
@@ -1,11 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mathexpr;
-
-import org.graalvm.polyglot.Context;
 
 /**
  *
@@ -14,7 +7,6 @@ import org.graalvm.polyglot.Context;
 public class MathMain {
     public static void main(String[] args) {
         String exp = "12+34+56";
-        Context cont = Context.create("mathlang");
-        System.out.println(cont.eval("mathlang", exp));
+        MathCommand.main(new String[]{exp});
     }
 }
